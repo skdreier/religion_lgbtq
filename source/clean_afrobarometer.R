be27e9e0f689d4b750eb6500d53de202fff91419
SKDreier <skdreier@uw.edu>
2019-06-12 09:28:11 -0700

    adding clean/descr source R code'

---
+++ source/clean_afrobarometer.R
@@ -0,0 +1,231 @@
+###############################################
+# Replication Code for:                       #
+# Long, Dreier, Winkler (P&R)                 #
+#                                             #
+# CODE TO CLEAN & TRANSFORM AFROBAROMETER     #
+# AFROBAORMETER DATA                          #
+# R version 3.5.2 (2018-12-20)                #
+# DATE: 5/17/2019                             #
+###############################################
+
+
+# Content:
+# - Load Afrobarometer dataset with added Herfindahl scores
+# - Clean and transform variables used in analysis
+# - Subset variables used in analysis into a new dataset
+# - Save that subsetted dataset
+
+rm(list=ls())
+
+#####################
+### LOAD PACKAGES ###
+#####################
+
+library(magrittr) #pipe function
+library(dplyr) #transformations
+library(forcats) #factor recoding
+
+##################################
+### LOAD AFROBAROMETER DATA W/ ###  
+### HERFINDAHL SCORES ############
+##################################
+
+afro <- read.csv("data/afro_with_herf.csv", header = TRUE)
+
+#################################
+### CLEAN/TRANSFORM VARIABLES ###
+#################################
+
+# Tolerance of Homosexuality (ordinal & logical)
+afro %<>%
+  mutate(sexuality = Q89C) # rename the ordinal version to something meaningful
+afro$sexuality[afro$sexuality==9] <- NA # drop don't know (n = 710)
+afro$sexuality[afro$sexuality==-1] <- NA # drop missing (n = 33)
+
+afro %<>%
+  dplyr::mutate(sexuality2 = as.factor(sexuality), # create a logical version of the variable
+                sexuality2 = fct_recode(sexuality2,
+                                        "0" = "1", # strongly dislike 
+                                        "0" = "2", # and somewhat dislike are set to 0
+                                        "1" = "3", # would not care
+                                        "1" = "4", # somewhat like
+                                        "1" = "5")) # and strongly like are set to 1
+
+afro$tol_sex2 <- as.numeric(afro$sexuality2 ) -1 # Replicate this with a name that matches other tolerance variables
+                                                 # this is used later in this clean script to create aggregate variables. 
+
+# Religious Tolerance (ordinal and logical)
+afro %<>%
+  mutate(tol_relig = Q89A) # create meaningful name
+afro$tol_relig[afro$tol_relig==9 | afro$tol_relig==-1] <- NA #drop don't know or missing (n = 232 + 24)
+
+afro %<>%
+  dplyr::mutate(tol_relig2 = as.factor(tol_relig), # create a logical version of the variable
+                tol_relig2 = fct_recode(tol_relig2,
+                                        "0" = "1",
+                                        "0" = "2",
+                                        "1" = "3",
+                                        "1" = "4",
+                                        "1" = "5"))
+afro$tol_relig2 <- as.numeric(afro$tol_relig2 ) -1 #convert to numeric
+
+# Ethnic Tolerance (ordinal and logical)
+afro %<>%
+  mutate(tol_ethnic = Q89B) # create meaningful name
+afro$tol_ethnic[afro$tol_ethnic==9 | afro$tol_ethnic==-1] <- NA #drop don't know or missing (n = 234 + 34)
+
+afro %<>%
+  dplyr::mutate(tol_ethnic2 = as.factor(tol_ethnic), # create logical version of variable
+                tol_ethnic2 = fct_recode(tol_ethnic2,
+                                         "0" = "1",
+                                         "0" = "2",
+                                         "1" = "3",
+                                         "1" = "4",
+                                         "1" = "5"))
+afro$tol_ethnic2 <- as.numeric(afro$tol_ethnic2 ) -1 #convert to numeric
+
+# HIV/AIDS Tolerance (ordinal and logical)
+afro %<>%
+  mutate(tol_hiv = Q89D) # create meaningful name
+afro$tol_hiv[afro$tol_hiv==9 | afro$tol_hiv==-1] <- NA #drop don't know or missing (n = 405 + 40)
+afro %<>%
+  dplyr::mutate(tol_hiv2 = as.factor(tol_hiv), #create logical version of the variable 
+                tol_hiv2 = fct_recode(tol_hiv2,
+                                      "0" = "1",
+                                      "0" = "2",
+                                      "1" = "3",
+                                      "1" = "4",
+                                      "1" = "5"))
+afro$tol_hiv2 <- as.numeric( afro$tol_hiv2 ) -1
+
+# Immigrant or foreign worker Tolerance (ordinal and logical)
+afro %<>%
+  mutate(tol_immig = Q89E) #create meaningful name 
+afro$tol_immig[afro$tol_immig==9  | afro$tol_immig==-1] <- NA #drop don't know or missing (n = 428 + 45)
+afro %<>%
+  dplyr::mutate(tol_immig2 = as.factor(tol_immig), # create logical version of the variable
+                tol_immig2 = fct_recode(tol_immig2,
+                                        "0" = "1",
+                                        "0" = "2",
+                                        "1" = "3",
+                                        "1" = "4",
+                                        "1" = "5"))
+afro$tol_immig2 <- as.numeric(afro$tol_immig2 ) -1 # convert to numeric 
+
+# Tolerance aggregates
+# Create a variable that aggregates an individual's tolerance of all social outgroups
+# Then, for each out-group, create an aggregate for all other social outgroups. 
+# This is used when we run placebo tests in the analysis and need to control for tolerance of all other outgroups.
+afro$tol_all <- afro$tol_relig2 + afro$tol_ethnic2 + afro$tol_sex2 + afro$tol_hiv2 + afro$tol_immig2
+afro$tol_noLGBT <- afro$tol_relig2 + afro$tol_ethnic2 + afro$tol_hiv2 + afro$tol_immig2
+afro$tol_noRelig <- afro$tol_ethnic2 + afro$tol_sex2 + afro$tol_hiv2 + afro$tol_immig2
+afro$tol_noEthnic <- afro$tol_relig2 + afro$tol_sex2 + afro$tol_hiv2 + afro$tol_immig2
+afro$tol_noHiv <- afro$tol_relig2 + afro$tol_ethnic2 + afro$tol_sex2 + afro$tol_immig2
+afro$tol_noImmig <- afro$tol_relig2 + afro$tol_ethnic2 + afro$tol_sex2 + afro$tol_hiv2
+
+# Age
+afro %<>%
+  mutate(age = as.numeric(Q1))
+afro$age[afro$age=="-1"] <- NA #drop missing (62)
+afro$age[afro$age=="998"] <- NA #drop resfused (25)
+afro$age[afro$age=="999"] <- NA #drop don't know (199)
+
+# Gender
+afro %<>%
+  mutate(female = Q101 %in% 2)
+
+# Education
+afro %<>%
+  mutate(education = Q97) # give it a meaningful name 
+afro$education[afro$education=="-1"] <- NA #drop missing (n=43)
+afro$education[afro$education=="99"] <- NA #drop don't know (n=86)
+afro$education <- as.factor(afro$education)
+
+# News from Radio
+afro %<>%
+  mutate(radio = Q12A)
+afro$radio[afro$radio=="-1"] <- NA #missing (n=33)
+afro$radio[afro$radio=="9"] <- NA #don't know (n=757)
+afro$radio <- as.factor(afro$radio)
+
+# News from TV
+afro %<>%
+  mutate(tv = Q12B)
+afro$tv[afro$tv=="-1"] <- NA #missing (n=33)
+afro$tv[afro$tv=="9"] <- NA #don't know (n=757)
+afro$tv <- as.factor(afro$tv)
+
+# News from Newspaper
+afro %<>%
+  mutate(newspaper = Q12C)
+afro$newspaper[afro$newspaper=="-1"] <- NA #missing (n=33)
+afro$newspaper[afro$newspaper=="9"] <- NA #don't know (n=757)
+afro$newspaper <- as.factor(afro$newspaper)
+
+# News from Internet
+afro %<>%
+  mutate(internet = Q12D)
+afro$internet[afro$internet=="-1"] <- NA #missing (n=33)
+afro$internet[afro$internet=="9"] <- NA #don't know (n=757)
+afro$internet <- as.factor(afro$internet)
+
+# Urban
+afro %<>%
+  mutate(urban = URBRUR) # give meaningful name
+afro$urban[afro$urban=="3"] <- .5 # transform peri-urban so it works within numerical order (n=512, all Botswana)
+afro$urban[afro$urban=="460"] <- NA # drop missing (n=88)
+afro$urban[afro$urban=="1"] <- 1 #Urban
+afro$urban[afro$urban=="2"] <- 0 #Rural
+
+# Wealth
+afro %<>%
+  mutate(water_access = Q93A)
+afro$water_access[afro$water_access=="-1"] <- NA #missing (n=61)
+afro$water_access[afro$water_access=="1"] <- 1 #inside house
+afro$water_access[afro$water_access=="2"] <- 1 #inside compound
+afro$water_access[afro$water_access=="3"] <- 0 #outside compound
+afro$water_access[afro$water_access=="9"] <- NA #don't know (n=148)
+afro$water_access[afro$water_access=="98"] <- NA #refused (n=3)
+
+# Religiosity
+afro %<>%
+  mutate(religiosity = Q98B)
+afro$religiosity[afro$religiosity=="-1"] <- NA #missing (n=577)
+afro$religiosity[afro$religiosity=="7"] <- 0 #has no religion
+afro$religiosity[afro$religiosity=="9"] <- NA #don't know (n=293)
+afro$religiosity[afro$religiosity=="98"] <- NA #(n=1)
+
+# rename county to district, which is less likely to be confused with Country
+afro <- dplyr::rename(afro, herf_relig_dist=herf_relig_cty)
+afro <- dplyr::rename(afro, herf_relig_bin_dist=herf_relig_bin_cty)
+afro <- dplyr::rename(afro, herf_ethn_dist=herf_ethn_cty)
+afro <- dplyr::rename(afro, herf_pol_dist=herf_pol_cty)
+afro <- dplyr::rename(afro, maj_relig_dist=maj_relig_cty)
+afro <- dplyr::rename(afro, maj_relig_bin_dist=maj_relig_bin_cty)
+afro <- dplyr::rename(afro, maj_ethn_dist=maj_ethn_cty)
+afro <- dplyr::rename(afro, maj_pol_dist=maj_pol_cty)
+afro <- dplyr::rename(afro, sample_size_dist=size_sample_cty)
+afro <- dplyr::rename(afro, sample_size_reg=size_sample_reg)
+afro <- dplyr::rename(afro, DISTRICT=COUNTY)
+
+# Subset data to only what we need for analysis
+afro <-
+  dplyr::select(afro, RESPNO, ethn, relig, relig_bin, COUNTRY, URBRUR, REGION, DISTRICT,
+                herf_ethn_reg, herf_ethn_dist, maj_ethn_reg, maj_ethn_dist,
+                herf_relig_reg, herf_relig_dist, maj_relig_reg, maj_relig_dist,
+                herf_relig_bin_reg, herf_relig_bin_dist, maj_relig_bin_reg, maj_relig_bin_dist,
+                herf_pol_reg, herf_pol_dist, maj_pol_reg, maj_pol_dist,
+                sexuality, sexuality2, age, female, education, urban,
+                water_access, religiosity, radio, tv, newspaper, internet, relig_bin,
+                christian, muslim, christ.mus,
+                tol_relig, tol_ethnic, sexuality, tol_hiv, tol_immig,
+                tol_relig2, tol_ethnic2, tol_sex2, tol_hiv2, tol_immig2,
+                tol_all, tol_noRelig, tol_noEthnic, tol_noLGBT,
+                tol_noHiv, tol_noImmig, sample_size_dist, sample_size_reg
+  )
+
+###############################
+###  SAVE SUBSETTED DATASET ###
+###############################
+
+write.csv(afro, file = "data/clean_afrobarometer.csv")
---
+++ source/descriptive_stats.R
@@ -0,0 +1,306 @@
+###############################################
+# Replication Code for:                       
+# Long, Dreier, Winkler (P&R)                 
+# 
+# Code to generate descriptive tables/figures.
+# 
+# OUTPUTS:
+#   - Figure 2a & 2b                        
+#   - Figure 3a & 3b
+#   - Table A.2 
+#   - Table A.3
+
+# AFROBAORMETER DATA                          
+# R version 3.5.2 (2018-12-20)                
+# DATE: 5/17/2019                             
+###############################################
+
+rm(list=ls())
+
+#####################
+### LOAD PACKAGES ###
+#####################
+
+library(ggplot2)
+library(dplyr)   
+library(magrittr)
+library(forcats)
+library(stargazer)
+library(scales)
+library(xtable)
+library(MASS)
+
+##########################
+### LOAD SUBSET OF     ###
+### AFROBAROMETER DATA ###
+##########################
+
+# setwd("~/Dropbox/Projects/Af_LGBT_Tolerance_JL_SW_SD")
+
+myData <- read.csv("data/clean_afrobarometer.csv", header = TRUE) 
+
+# Invert Herf so that E(0:1) where 1 is more heterogenous district
+myData$herf_ethn_reg <- 1.00 - myData$herf_ethn_reg
+myData$herf_ethn_dist <- 1.00 - myData$herf_ethn_dist
+myData$herf_relig_reg <- 1.00 - myData$herf_relig_reg
+myData$herf_relig_dist <- 1.00 - myData$herf_relig_dist
+myData$herf_relig_bin_reg <- 1.00 - myData$herf_relig_bin_reg
+myData$herf_relig_bin_dist <- 1.00 - myData$herf_relig_bin_dist
+myData$herf_pol_reg <- 1.00 - myData$herf_pol_reg
+myData$herf_pol_dist <- 1.00 - myData$herf_pol_dist
+
+###################################
+### FIGURE 2A: DISTRIBUTION OF ####
+### UNBINNED DEPENDENT VARIABLE ###
+###################################
+
+myData %<>% 
+  mutate(sexuality4 = as.factor(sexuality), #Create new variable
+         sexuality4 = fct_recode(sexuality4,#and rename responses so they are easier to identify in plot
+                                 "Strongly\nDislike" = "1",
+                                 "Somewhat\nDislike" = "2",
+                                 "Don't\nCare" = "3",
+                                 "Somewhat\nLike" = "4",
+                                 "Strongly\nLike" = "5"))
+
+dv_plot2 <- #plot for ordinal dependent variable (Figure 2a)
+  ggplot(data = subset(myData, !is.na(sexuality4)), aes(x = as.factor(sexuality4))) + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  xlab("") + ylab("") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 2)) +
+  theme(axis.text.x = element_text(size=16), 
+        axis.text.y = element_text(size=16)) +
+  theme_bw()
+
+ggsave(filename = "plots/dv_plot2.png", #Save it to directory
+       plot = dv_plot2)
+
+###################################
+### FIGURE 2B: DISTRIBUTION OF ####
+### BINNED DEPENDENT VARIABLE #####
+###################################
+
+myData %<>% 
+  mutate(sexuality3 = as.factor(sexuality2), #Create new variable 
+         sexuality3 = fct_recode(sexuality3, #and rename responses so they are easier to identify in plot
+             "Strongly or Somewhat\nDislike" = "0",
+             "Strongly or Somewhat\nLike or Don't Care" = "1"))
+
+dv_plot1 <- #plot for logical dependent variable (Figure 2b) 
+  ggplot(data = subset(myData, !is.na(sexuality3)), aes(x = as.factor(sexuality3))) + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  xlab("") + ylab("") + 
+  scale_y_continuous(labels = scales::percent_format(accuracy = 2)) +
+  theme(axis.text.x = element_text(size=16), 
+        axis.text.y = element_text(size=16)) +
+  theme_bw()
+  
+ggsave(filename = "plots/dv_plot1b.png", #Save it to directory
+       plot = dv_plot1)
+
+#####################################
+### FIGURE 3A: DISTRIBUTION OF ######
+### UNBINNED RELIGIOUS HERFINDAHL ###
+#####################################
+
+sub <- # subset to only the unique districts
+  unique(data.frame(myData$DISTRICT, myData$herf_relig_dist, myData$herf_relig_bin_dist))
+
+iv_plot1 <- # plot independent variable based on original religion categories (Figure 3a)
+  ggplot(data = sub, aes(x = myData.herf_relig_dist)) +
+  geom_histogram() + 
+  xlab("Inverse Religious HHI (0 = homogeneous)\n(original religion categories)") +
+  ylab("") +
+  theme(axis.text.x = element_text(size=14), 
+        axis.title.x = element_text(size=14), 
+        axis.text.y = element_text(size=14),
+        axis.title.y = element_text(size=14)) + 
+  theme_bw()
+
+ggsave(filename = "plots/iv_plot1_dist.png", #Save it to directory
+       plot = iv_plot1)
+
+#####################################
+### FIGURE 3B: DISTRIBUTION OF ######
+### BINNED RELIGIOUS HERFINDAHL #####
+#####################################
+
+iv_plot2 <- # plot independent variable based on binned religion categories (Figure 3b)
+  ggplot(data = sub, aes(x = myData.herf_relig_bin_dist)) +
+  geom_histogram() + 
+  xlab("Inverse Religious HHI (0 = homogeneous)\n(binned religion categories)") +
+  ylab("") +
+  theme(axis.text.x = element_text(size=14), 
+        axis.title.x = element_text(size=14), 
+        axis.text.y = element_text(size=14),
+        axis.title.y = element_text(size=14)) +
+  theme_bw()
+
+ggsave(filename = "plots/iv_plot2_dist.png", #Save it to directory
+       plot = iv_plot2)
+
+#######################################
+### TABLE A.2: COVARIANCE MATRIX OF ###
+### ALL SOCIAL TOLERANCE VARAIBLES ####
+#######################################
+
+tolerance_data <- select(myData, tol_relig, tol_ethnic, sexuality,
+                         tol_hiv, tol_immig)
+tolerance_cov_table <-
+  cor(tolerance_data, use = "pairwise.complete.obs", method = "spearman") 
+
+row_names <- c("Religion", "Ethnicity", "Homosexual", "HIV", "Immigrant") #create meaningful row/col names
+col_names <- c("Religion", "Ethnicity", "Homosexual", "HIV", "Immigrant")
+rownames(tolerance_cov_table) <- row_names
+colnames(tolerance_cov_table) <- col_names
+
+stargazer(tolerance_cov_table, title="Spearman Correlation Matrix of Tolerance Battery") # latex output
+
+####################################
+### TABLE A.3: DESCRIPTIVE STATS ###
+### FOR MODEL COVARIATES ###########
+####################################
+
+table_data <- myData[,c("sexuality2", # setup table
+                        "herf_relig_bin_dist", "maj_relig_bin_dist",
+                        "herf_ethn_dist", "maj_ethn_dist",  
+                        "christian", "muslim",  
+                        #Controls
+                        "tol_relig2", "tol_ethnic2", "tol_hiv2", "tol_immig2",
+                        "female", "age", "education", "urban", "religiosity",
+                        "water_access", "internet",
+                        #Robustness
+                        "sexuality",
+                        "herf_relig_bin_reg", "maj_relig_bin_reg",
+                        "herf_ethn_reg", "maj_ethn_reg",
+                        "herf_relig_dist", "maj_relig_dist")]
+
+# Function to generate summary statistics
+stat_sum <- function(vec) {
+  out <- c ( min(vec, na.rm=T), mean(vec, na.rm=T), max(vec, na.rm=T), sd(vec, na.rm=T)
+  )
+  return(out)
+}
+
+# Apply function to stats
+tab_sum <- round ( t ( apply(table_data, 2, stat_sum) ), 2)
+
+# Label Columns 
+colnames(tab_sum) <- c("Min", "Mean", "Max", "S.D.")
+
+# Label Rows w var names
+row.names(tab_sum) <- c(
+  "LGBT Tolerance (0=intolerant)", 
+  "Religious HHI (district)", "Religious Majority (district, 0=minority)",
+  "Ethnic HHI (district)", "Ethnic Majority (district, 0=minority)",  
+  "Christian (0=non-Christian)", "Muslim (0=non-Muslim)",  
+  
+  "Religious Tolerance (0=intolerant)", "Ethnic Tolerance (0=intolerant)", 
+  "HIV Tolerance (0=intolerant)", "Immigrant Tolerance (0=intolerant)",
+  "Female (0=male)", "Age", "Education Level", "Urban (0=rural)", "Religiosity",
+  "Water Access", "Internet Access",
+  "LGBT Tolerance (scale)", 
+  "Religious HHI (region)", "Religious Majority (region)",
+  "Ethnic HHI (region)", "Ethnic Majority (region)",
+  "Raw Religious HHI (district)", "Raw Religious Majority (district)"
+)
+
+xtable(tab_sum) #latex output 
+
+
+
+#####################################################
+### FIGURE 1: % DISLIKE LGBTQ NEIGHBOR BY COUNTRY ###
+### DESCRIPTIVE STATISTICS FROM AFROB ###############
+#####################################################
+
+# Label country names for plot 
+myData %<>%
+  dplyr::mutate(ctry = as.factor(COUNTRY), #create logical version of the variable 
+                ctry = fct_recode(ctry,
+                                  "Benin" = "2",
+                                  "Botswana" = "3",
+                                  "Burkina Faso" = "4",
+                                  "Burundi"  = "5",
+                                  "Cameroon"  = "6",
+                                  "Cape Verde"  = "7",
+                                  "Cote d'Ivoire"  = "8",
+                                  "Gabon"  = "10",
+                                  "Ghana"  = "11",
+                                  "Guinea"  = "12",
+                                  "Kenya"  = "13",
+                                  "Lesotho"  = "14",
+                                  "Liberia"  = "15",
+                                  "Madagascar"  = "16",
+                                  "Malawi"  = "17",
+                                  "Mali"  = "18",
+                                  "Mauritius"  = "19",
+                                  "Morocco"  = "20",
+                                  "Mozambique"  = "21",
+                                  "Namibia"  = "22",
+                                  "Niger"  = "23",
+                                  "Nigeria"  = "24",
+                                  "Sao Tome & Principe"  = "25",
+                                  "Senegal"  = "26",
+                                  "Sierra Leone"  = "27",
+                                  "South Africa"  = "28",
+                                  "Swaziland"  = "30",
+                                  "Tanzania" = "31",
+                                  "Togo" = "32",
+                                  "Tunisia" = "33",
+                                  "Uganda" = "34",
+                                  "Zambia" = "35",
+                                  "Zimbabwe" = "36"  ),
+                ctry = as.character(ctry)
+                )
+
+# Omit NAs on sexuality2 question for mean calculations
+meanData <- na.omit(data.frame(myData$ctry, myData$sexuality2))
+colnames(meanData) <- c("ctry", "sexuality2")
+
+# Create "All survey" variable to calculate survey mean
+all_survey <- meanData
+all_survey$ctry <- "SURVEY MEAN"
+meanData <- rbind(meanData, all_survey)
+
+# Split dataset by country
+country_list <- split(meanData, meanData$ctry)
+
+# Function to caluclate country means/CI
+mean_func <- function(data) {
+  n = length(data$sexuality2)
+  sigma = sd(data$sexuality2)
+  sem = sigma/sqrt(n)
+  E = qnorm(.975)*sem 
+  xbar = 1-mean(data$sexuality2, na.rm=T)
+  xbar + c(-E, 0, E)
+}
+
+# Apply function to each country in country_list
+country_list_means <- lapply(country_list, mean_func)
+
+# Collapse list into single dataset
+data_out <- t(bind_cols(country_list_means))
+
+# Prepare dataset for plotting
+plot_data <- data.frame( rownames(data_out), data_out, row.names=NULL, stringsAsFactors = F)
+colnames(plot_data) <- c("ctry", "lower", "mean", "upper")
+sorted <- plot_data[ rev(order(plot_data$mean)) ,]
+row.names(sorted) = c(1:34)
+
+# PLOT FIGURE 1: Percent who would dislike having an LGBTQ neighbor (by country)
+#pdf(file="plots/lgbt_ctry_perc.pdf",width=5,height=5)
+par(mai=c(.7,1,0.35,1.2))
+plot(sorted$mean, nrow(sorted):1, type="p", xlim =c(0,1), ylim=c(0,34),
+     pch=16, cex=.7, yaxt="n", xaxt="n", ylab="", xlab="", bty="n", cex.main=.9, cex.axis=.9)
+abline(h= abs(length(sorted$ctry)-which(sorted$ctry=="SURVEY MEAN"))+1, col="grey")
+for (i in 1:length(sorted$ctry)){
+  segments(x0= sorted$lower[i], y0=abs(i-length(sorted$ctry))+1, 
+           x1= sorted$upper[i], y1=abs(i-length(sorted$ctry))+1  )
+}
+points(0.7823729, 9, pch=16, cex=.7)
+axis(4, at=1:34, labels = rev(sorted$ctry), las=1, cex.axis=.6) 
+axis(1, at=c(seq(0,1,.2)), labels = c("0%", "20%", "40%", "60%", "80%", "100%"), las=1, cex.axis=.8) 
+#dev.off()
+
+
