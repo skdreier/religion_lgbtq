648bab640c68d1bdac3810ee165dc8180e187a09
SKDreier <skdreier@uw.edu>
2019-06-18 08:48:07 -0700

    adding code and plots for country-specific analysis

--- source/5_model_plots.R
+++ source/5_model_plots.R
@@ -7,11 +7,12 @@
 # OUTPUTS:                                    #
 #   - Figure 4                                #
 #   - Figure 5A and 5B                        #
+#   - Figures A.25 (Country plots)            #
 #                                             #
 # AFROBAORMETER DATA                          #  
 # R version 3.6.0 (2019-04-26)                #
 #                                             #
-# DATE: 06/17/2019                            # 
+# DATE: 06/18/2019                            # 
 ###############################################
 
 rm(list=ls())
@@ -164,6 +165,123 @@ ggplot(data=plot_data_full, aes(x = herf_relig_bin_dist)) + # xlim(0,.9) + ylim(
   annotate("text", x = .38, y=.08, label = "Pr (Not Opposing LGBT Neighbors)", color="blue", size=5.5) +
   ggsave("figures/5b_model3_ordered_tolerate.png", device="png")
 
+
+######################################################################
+### FIGURES A.25 #####################################################    
+### MODEL 3 Simulation: Probability of tolerating LGBTs by country ###
+### AFROBAROMETER DATA ###############################################
+######################################################################
+
+# Model to loop over for most countries in analysis
+model <- 
+  as.factor(sexuality2) ~ herf_relig_bin_dist + 
+  maj_relig_bin_dist + christian + muslim +
+  female + age + education + water_access + urban + religiosity + internet +
+  tol_relig2 + tol_ethnic2 + tol_hiv2 + tol_immig2 
+
+# Drop countries whose religious demographics make Model 3 over-determined
+data_sub <- subset(myData, ctry!="Morocco" & ctry!="Mali" & ctry!="Niger" & ctry!="Sao Tome and Principe" & ctry!="Tunisia" & ctry!="Senegal" & ctry!="Sierra Leone") 
+
+# Loop over each country to estimate model and plot simulation
+countrylist <- unique(data_sub$ctry)
+n <- length(countrylist)
+
+for (i in 1:length(countrylist)) {
+  
+  currcty <- countrylist[i]
+  
+  mdata_0 <- subset(data_sub, ctry==currcty)
+  mdata <- extractdata(model, mdata_0, na.rm=TRUE) 
+  zmod <- zelig(model, mdata, model="logit")
+  
+  herf_range <- seq(min(mdata$herf_relig_bin_dist, na.rm=T), max(mdata$herf_relig_bin_dist, na.rm=T), length.out=1000)
+  suppressWarnings( x.out <- setx(zmod, herf_relig_bin_dist = herf_range) )
+  suppressWarnings( s.out <- Zelig::sim(zmod, x=x.out) )
+  
+  plotdata <- mv_extract(s.out)
+  plot_data_full <- data.frame(plotdata, herf_relig_bin_dist = herf_range)
+  
+  ggplot(data=plot_data_full, aes(x = herf_relig_bin_dist)) + 
+    geom_line(aes(y =mean)) +
+    geom_line(aes(y =high), linetype="dashed", color = "blue") +
+    geom_line(aes(y =low), linetype="dashed", color = "blue") +
+    theme_minimal() +
+    
+    labs(
+          title = paste(currcty, "Effect of Religious Diversity", sep=": "),
+          subtitle = paste("on toleration of sexual minorities (logit)"),
+          x = paste(xlab_name, "(0 = homogeneous)", sep=" "),
+          y = "Pr (Tolerating LGBT neighbors)"  
+          ) +
+    
+    theme(
+          plot.title = element_text(size=16),
+          plot.subtitle = element_text(size=15),
+          axis.text.x = element_text(size=14), 
+          axis.title.x = element_text(size=14), 
+          axis.text.y = element_text(size=14),
+          axis.title.y = element_text(size=14)
+          ) +
+    
+    ggsave( paste("figures/ctry_plots/", currcty, "_model3.png", sep=""), device="png" )
+  
+}
+
+# Model for countries omitted above (model removes Christian and Muslim binary variables):
+model <- 
+  as.factor(sexuality2) ~ herf_relig_bin_dist + 
+  maj_relig_bin_dist + #christian + muslim +
+  female + age + education + water_access + urban + religiosity + internet +
+  tol_relig2 + tol_ethnic2 + tol_hiv2 + tol_immig2 
+
+# Subset to relevant countries
+data_sub2 <- subset(myData, ctry=="Mali" | ctry=="Niger" | 
+                 ctry=="São Tomé and Príncipe" | ctry=="Tunisia" | ctry=="Senegal" | 
+                 ctry=="Sierra Leone") # Morocco omitted
+
+# Loop over each country to estimate model and plot simulation
+countrylist <- unique(data_sub2$ctry)
+n <- length(countrylist)
+
+for (i in 1:length(countrylist)) {
+  currcty <- countrylist[i]
+  
+  mdata_0 <- subset(data_sub2, ctry==currcty)
+  mdata <- extractdata(model, mdata_0, na.rm=TRUE) 
+  zmod <- zelig(model, mdata, model="logit")
+  
+  herf_range <- seq(min(mdata$herf_relig_bin_dist, na.rm=T), max(mdata$herf_relig_bin_dist, na.rm=T), length.out=1000)
+  suppressWarnings( x.out <- setx(zmod, herf_relig_bin_dist = herf_range) )
+  suppressWarnings( s.out <- Zelig::sim(zmod, x=x.out) )
+  
+  plotdata <- mv_extract(s.out)
+  plot_data_full <- data.frame(plotdata, herf_relig_bin_dist = herf_range)
+  
+  ggplot(data=plot_data_full, aes(x = herf_relig_bin_dist))  + #ylim(.03,.08) + xlim(0,.9) +
+    geom_line(aes(y =mean)) +
+    geom_line(aes(y =high), linetype="dashed", color = "blue") +
+    geom_line(aes(y =low), linetype="dashed", color = "blue") +
+    theme_minimal() +
+    
+    labs(
+      title = paste(currcty, "Effect of Religious Diversity", sep=": "),
+      subtitle = paste("on toleration of sexual minorities (logit)"),
+      x = paste(xlab_name, "(0 = homogeneous)", sep=" "),
+      y = "Pr (Tolerating LGBT neighbors)"  
+      ) +
+    
+    theme(
+      plot.title = element_text(size=16),
+      plot.subtitle = element_text(size=15),
+      axis.text.x = element_text(size=14), 
+      axis.title.x = element_text(size=14), 
+      axis.text.y = element_text(size=14),
+      axis.title.y = element_text(size=14)
+      ) +
+    
+    ggsave( paste("figures/ctry_plots/", currcty, "_model3.png", sep=""), device="png" )
+  
+}
 
 ################################################
 ########       END OF PLOT SCRIPT       ########
